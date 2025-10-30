3cd1d02fc72283a446138ee495f35df96388e057
solomon-211 <s.leek@alustudent.com>
2025-10-30 21:54:34 +0000

    1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,20 +8,17 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
-    if not subreddit or not isinstance(subreddit, str):
-        print(None)
-        return
-
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "linux:api_advanced:v1.0 (by /u/solomon/"}
+    headers = {"User-Agent": "linux:api_advanced:v1.0 (by /u/solomon)"}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json().get("data", {}).get("children", [])
-            for post in data:
-                print(post.get("data", {}).get("title"))
-        else:
-            print(None)
+        if response.status_code != 200:
+            return  # Do nothing for invalid subreddit
+
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
     except Exception:
-        print(None)
+        pass
